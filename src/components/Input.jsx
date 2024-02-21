e6b0909024793a884e5609cf573804903b37398a
Your Name <nihaal@sapnagroup.com>
2024-02-21 17:58:46 +0100

    Initial/Final commit

---
+++ src/components/Input.jsx
@@ -0,0 +1,21 @@
+export default function Input({ label, invalid, ...props }) {
+
+    let labeClasses = "block mb-2 text-xs font-bold tracking-wide uppercase";
+    let inputClasses = "w-full px-3 py-2 leading-tight border rounded shadow";
+
+    if(invalid) {
+        labeClasses += " text-red-400";
+        inputClasses += " text-red-500 bg-red-100 border-red-300";
+    } else {
+        labeClasses += " text-stone-300"
+        inputClasses += " text-gray-700 bg-stone-300";
+    }
+
+
+    return (
+        <p>
+            <label className={labeClasses}>{label}</label>
+            <input className={inputClasses} {...props} />
+        </p>
+    );
+};
